b770afc43a62d20252bef50177c450f05d92f6cc
AllanMicuanski <allanmicuanski@gmail.com>
2024-10-10 17:25:25 -0300

    por today deu

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const puppeteer = require("puppeteer");
 const helmet = require("helmet");
 const path = require("path");
 
-const app = express(); // Aqui você define o 'app'
+const app = express();
 const port = 3000;
 
 // Middleware para servir arquivos estáticos
-app.use(express.static(path.join(__dirname))); // Adicione isso
+app.use(express.static(path.join(__dirname)));
 
 app.use(helmet());
 
@@ -19,26 +19,38 @@ app.get("/", (req, res) => {
 // Rota para verificar URL
 app.get("/api/verificar", async (req, res) => {
   const url = req.query.url;
-
   if (!url) {
     return res.status(400).json({ error: "URL não fornecida." });
   }
 
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const requisitions = [];
+  const deploymentStatus = {
+    script: false,
+    gtm: false,
+    vtexIO: false,
+  };
 
-    const requisitions = [];
-    const deploymentStatus = {
-      script: false,
-      gtm: false,
-      vtexIO: false,
-    };
+  // Interceptar requisições
+  await page.setRequestInterception(true);
+  page.on("request", (request) => {
+    const requestUrl = request.url();
+    const initiator = request.initiator(); // Obtenha o initiador da requisição
 
-    // Capturar requisições de rede
-    page.on("request", (request) => {
-      const requestUrl = request.url();
+    // Log o initiator para verificar suas propriedades
+    if (initiator) {
+      console.log("Initiator:", initiator);
 
+      if (initiator.type === "script") {
+        console.log("Iniciador do tipo script encontrado.");
+      }
+    } else {
+      console.log("Initiator não definido.");
+    }
+
+    // Verifique se o initiator tem uma propriedade `url`
+    if (initiator && initiator.url) {
       // Filtra apenas requisições que contenham "sizebay"
       if (requestUrl.includes("sizebay")) {
         console.log(`Capturado (sizebay): ${requestUrl}`);
@@ -61,42 +73,44 @@ app.get("/api/verificar", async (req, res) => {
           );
         }
       }
-    });
-
-    // Navegar para a URL
-    await page.goto(url, { waitUntil: "networkidle2" });
-
-    // Usar delay no Node.js para esperar por requisições adicionais (ex: 10 segundos)
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-    console.log("Esperando requisições...");
-
-    // Verifica se o Sizebay está presente
-    const permalink = await page.evaluate(() => {
-      return window.SizebayPrescript
-        ? window.SizebayPrescript().getPermalink()
-        : null;
-    });
-
-    // Nova verificação do script baseado no permalink
-    if (permalink && !deploymentStatus.gtm && !deploymentStatus.vtexIO) {
-      deploymentStatus.script = true;
-      console.log("Existe permalink, então é Script");
+    } else {
+      console.log("Initiator não possui URL ou está indefinido");
     }
 
-    await browser.close();
+    // Continue com a requisição
+    request.continue();
+  });
 
-    // Envia os dados obtidos de volta para o frontend
-    res.json({
-      requisitions,
-      scriptStatus: deploymentStatus.script,
-      gtmStatus: deploymentStatus.gtm,
-      vtexIOStatus: deploymentStatus.vtexIO,
-      permalink,
-    });
-  } catch (error) {
-    console.error("Erro ao verificar a URL:", error);
-    res.status(500).json({ error: "Erro ao processar a URL." });
+  await page.goto(url, { waitUntil: "networkidle2" });
+
+  // Usar delay no Node.js para esperar por requisições adicionais (ex: 10 segundos)
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  console.log("Esperando requisições...");
+
+  // Capturar permalink se disponível
+  const permalink = await page.evaluate(() => {
+    return window.SizebayPrescript
+      ? window.SizebayPrescript().getPermalink()
+      : null;
+  });
+
+  // Nova verificação do script baseado no permalink
+  if (permalink && !deploymentStatus.gtm && !deploymentStatus.vtexIO) {
+    deploymentStatus.script = true;
+    deploymentStatus.vtexIO = false;
+    deploymentStatus.gtm = false;
+
+    console.log("Existe permalink, então é Script");
   }
+
+  await browser.close();
+  res.json({
+    requisitions,
+    scriptStatus: deploymentStatus.script,
+    gtmStatus: deploymentStatus.gtm,
+    vtexIOStatus: deploymentStatus.vtexIO,
+    permalink,
+  });
 });
 
 // Inicia o servidor
