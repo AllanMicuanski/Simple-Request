50709404db8773ac24b890b0b56cc529e4ad06dd
AllanMicuanski <allanmicuanski@gmail.com>
2024-10-14 14:53:50 -0300

    fix:serveless func

---
+++ api/verificar.js
@@ -0,0 +1,72 @@
+const puppeteer = require("puppeteer");
+
+module.exports = async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: "URL não fornecida." });
+  }
+
+  try {
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    const requisitions = [];
+    const deploymentStatus = {
+      script: false,
+      gtm: false,
+      vtexIO: false,
+    };
+
+    await page.setRequestInterception(true);
+    page.on("request", (request) => {
+      const requestUrl = request.url();
+
+      // Captura requisições que contenham "sizebay"
+      if (requestUrl.includes("sizebay")) {
+        requisitions.push({
+          url: requestUrl,
+          method: request.method(),
+        });
+
+        // Verificação de VTEX IO
+        if (requestUrl.includes("vtex_module.js")) {
+          deploymentStatus.vtexIO = true;
+        }
+
+        // Verificação de GTM via prescript.js
+        if (requestUrl.includes("prescript.js")) {
+          deploymentStatus.gtm = true;
+        }
+      }
+      request.continue();
+    });
+
+    await page.goto(url, { waitUntil: "networkidle2" });
+
+    // Capturar permalink se disponível
+    const permalink = await page.evaluate(() => {
+      return window.SizebayPrescript
+        ? window.SizebayPrescript().getPermalink()
+        : null;
+    });
+
+    if (permalink && !deploymentStatus.gtm && !deploymentStatus.vtexIO) {
+      deploymentStatus.script = true;
+    }
+
+    await browser.close();
+
+    res.status(200).json({
+      requisitions,
+      scriptStatus: deploymentStatus.script,
+      gtmStatus: deploymentStatus.gtm,
+      vtexIOStatus: deploymentStatus.vtexIO,
+      permalink,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao processar a URL." });
+  }
+};
